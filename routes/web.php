36380466bf36139a2f45ff94956b1f1364e1c95f
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-03 15:36:56 +0700

    middleware,gate admin

--- routes/web.php
+++ routes/web.php
@@ -5,46 +5,45 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Welcome Route
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard Route
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profile Routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('todo', TodoController::class)->except(['show']);
+    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
 });
 
-Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+// Admin Routes
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('user', UserController::class)->except(['show']);
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+});
 
-Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+// // Todo Routes
+// Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+// Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+// Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+// Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+// Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
 
-Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+// // User Routes
+// Route::get('/user', [UserController::class, 'index'])->name('user.index');
+// Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 
-Route::get('/user', [UserController::class, 'index'])->name('user.index');
-
-Route::resource('todo', TodoController::class)->except(['show']);
-
-Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
-
-Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete' ])->name('todo.uncomplete');
-
-Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
-
-Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-
-Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-
-Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
-
-Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-
-Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
-
-Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-
+// Auth Routes
 require __DIR__.'/auth.php';
---
+++ tugas7.md
@@ -0,0 +1,7 @@
+# Tugas 7 - Screenshot Todo App
+
+## 1. Gate Admin Not Active 
+![alt text](screenshot/tugas7/Gate_Admin_Not_Active.png) 
+
+## 2. Gate Admin Active
+![alt text](screenshot/tugas7/Gate_Admin_Active.png)
